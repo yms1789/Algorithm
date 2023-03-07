aab902de8ac592db0cd0fc1aeb7ab3d942a15132
YMS <yms1789@naver.com>
2023-03-07 23:58:22 +0900

    23년 26일차 문제풀이 완료

---
+++ 23년 26일차/방문 길이.js
@@ -0,0 +1,26 @@
+function solution(dirs) {
+  let dx = [-1, 0, 1, 0]; // U L D R
+  let dy = [0, -1, 0, 1];
+  let visited = {};
+  let cur = [0, 0];
+  for (let i = 0; i < dirs.length; i++) {
+    let next = [];
+    if (dirs[i] == "U") {
+      next = [cur[0] - 1, cur[1]];
+    } else if (dirs[i] == "L") {
+      next = [cur[0], cur[1] - 1];
+    } else if (dirs[i] == "D") {
+      next = [cur[0] + 1, cur[1]];
+    } else if (dirs[i] == "R") {
+      next = [cur[0], cur[1] + 1];
+    }
+    if (next[0] < -5 || next[1] < -5 || next[0] > 5 || next[1] > 5) {
+      continue;
+    }
+
+    visited[[cur, next]] = true;
+    visited[[next, cur]] = true;
+    cur = next;
+  }
+  return Object.keys(visited).length / 2;
+}
