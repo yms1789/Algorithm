8b25107c1d906d1b5aa218779d6592f53c7e4d1f
YMS <yms1789@naver.com>
2023-05-16 19:09:10 +0900

    changing: 홈 방범 서비스 코드 수정

--- 23년 36일차/홈 방범 서비스.java
+++ 23년 36일차/홈 방범 서비스.java
@@ -6,84 +6,105 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-  static class Dir {
-    int x;
-    int y;
 
-    public Dir(int dx, int dy) {
-      this.x = dx;
-      this.y = dy;
-    }
-  }
+	static class Dir {
+		int x;
+		int y;
 
-  static int N, M;
-  static int[][] map;
-  static int[] dx = { -1, 1, 0, 0 };
-  static int[] dy = { 0, 0, -1, 1 };
-  static boolean[][] visited;
-  static Queue<Dir> queue;
-  static int res;
+		Dir(int dx, int dy) {
+			this.x = dx;
+			this.y = dy;
+		}
+	}
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int T = Integer.parseInt(br.readLine());
-    for (int tc = 1; tc <= T; tc++) {
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      N = Integer.parseInt(st.nextToken());
-      M = Integer.parseInt(st.nextToken());
-      map = new int[N][N];
-      res = Integer.MIN_VALUE;
-      for (int i = 0; i < N; i++) {
-        st = new StringTokenizer(br.readLine());
-        for (int j = 0; j < N; j++) {
-          map[i][j] = Integer.parseInt(st.nextToken());
-        }
-      }
+	static int[] dx = { 1, -1, 0, 0 };
+	static int[] dy = { 0, 0, 1, -1 };
+	static int N, M;
+	static int[][] map;
+	static boolean[][] visited;
+	static Queue<Dir> queue;
+	static int maxHouse;
 
-      for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-          queue = new LinkedList<>();
-          visited = new boolean[N][N];
-          bfs(i, j);
-        }
-      }
-      System.out.println("#" + tc + " " + res);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 
-  }
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
 
-  static void bfs(int i, int j) {
-    queue.offer(new Dir(i, j));
-    visited[i][j] = true;
-    int K = 1;
-    int house = map[i][j] == 1 ? 1 : 0;
-    if (getOperationCost(K) <= house * M) {
-      res = Math.max(K, res);
-    }
-    while (!queue.isEmpty()) {
-      K++;
-      int size = queue.size();
-      for (int k = 0; k < size; k++) {
+			map = new int[N][N];
+			maxHouse = 1;
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
 
-        Dir cur = queue.poll();
-        for (int n = 0; n < 4; n++) {
-          int nx = cur.x + dx[n];
-          int ny = cur.y + dy[n];
-          if (nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny])
-            continue;
-          if (map[nx][ny] == 1)
-            house++;
-          queue.add(new Dir(nx, ny));
-          visited[nx][ny] = true;
-        }
-      }
-      if (getOperationCost(K) <= house * M) {
-        res = Math.max(house, res);
-      }
-    }
-  }
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					visited = new boolean[N][N];
+					queue = new LinkedList<>();
+					bfs(i, j);
+				}
+			}
+			System.out.println("#" + tc + " " + maxHouse);
+		}
 
-  private static int getOperationCost(int k) {
-    return k * k + (k - 1) * (k - 1);
-  }
+	}
+
+	static void bfs(int x, int y) {
+		int K = 1;
+		int house = map[x][y];
+		visited[x][y] = true;
+		queue.offer(new Dir(x, y));
+		
+		while (!queue.isEmpty()) {
+			K++;
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				Dir cur = queue.poll();
+				int cx = cur.x;
+				int cy = cur.y;
+				for (int j = 0; j < 4; j++) { // 아래, 위, 오른쪽, 왼쪽
+					int nx = cx + dx[j];
+					int ny = cy + dy[j];
+					if (isWall(nx, ny) || visited[nx][ny]) {
+						continue;
+					}
+					queue.add(new Dir(nx, ny));
+					visited[nx][ny] = true;
+					if (map[nx][ny] == 1)
+						house++;
+
+				}
+			}
+			if (calcCost(K) <= M * house)
+				maxHouse = Math.max(maxHouse, house);
+		}
+	}
+
+	static boolean isWall(int x, int y) {
+		if (x < 0 || y < 0 || x >= N || y >= N) {
+			return true;
+		}
+		return false;
+	}
+
+	static int calcCost(int k) {
+		return (k * k) + (k - 1) * (k - 1);
+	}
+
+	static void printVisited() {
+		for (int i = 0; i < N; i++) {
+			System.out.println();
+			for (int j = 0; j < N; j++) {
+				System.out.print(visited[i][j] + " ");
+			}
+		}
+		System.out.println();
+
+	}
 }
