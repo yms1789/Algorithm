53ad8251e048e95b99e6a0ca871b735bb98e402c
여민수 <yms1789@naver.com>
2024-02-25 01:06:46 +0900

    New: 24년 21일차 문제풀이 완료

--- 24년 21일차/합 구하기.js
+++ 24년 21일차/합 구하기.js
@@ -4,24 +4,20 @@ const stdin = fs.readFileSync(filePath).toString().trim();
 
 let input = stdin.split("\n");
 
-let [N] = input[0];
-const arr = input[1].split(" ").map(Number);
-let [M] = input[2];
-[N, M] = [parseInt(N), parseInt(M)];
+let N = parseInt(input[0]);
+const arr = input[1].split(" ").map((ele) => Number(ele));
+let M = parseInt(input[2]);
 
-let pre = [arr[0]];
+let pre = Array(N + 1).fill(0);
+
 let result = [];
-
-for (let i = 1; i < arr.length; i++) {
-  pre[i] = pre[i - 1] + arr[i];
+for (let i = 1; i <= arr.length; i++) {
+  pre[i] = pre[i - 1] + arr[i - 1];
 }
 
-let gugan = input.splice(3).map((ele) => ele.split(" ").map(Number));
 for (let i = 0; i < M; i++) {
-  let a = gugan[i][0] - 1;
-  let b = gugan[i][1] - 1;
-  let sum = a === 0 ? pre[b] : pre[b] - pre[a - 1];
-
+  let [a, b] = input[i + 3].split(" ");
+  let sum = pre[b] - pre[a - 1];
   result.push(sum);
 }
 console.log(result.join("\n"));
